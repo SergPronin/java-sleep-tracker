e5e0d9cfcf1337e98d1af196b68440da67afa5ae
SergPronin <sergeypronin.dev@gmail.com>
2026-04-17 17:03:31 +0300

    добавлены парсеры

--- src/main/java/ru/yandex/practicum/sleeptracker/model/SleepingSession.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/model/SleepingSession.java
@@ -3,12 +3,12 @@ package ru.yandex.practicum.sleeptracker.model;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class SleepSession {
+public class SleepingSession {
   private LocalDateTime startTime;
   private LocalDateTime endTime;
   private SleepQuality sleepQuality;
 
-  public SleepSession(LocalDateTime startTime, LocalDateTime endTime, SleepQuality sleepQuality) {
+  public SleepingSession(LocalDateTime startTime, LocalDateTime endTime, SleepQuality sleepQuality) {
     this.startTime = startTime;
     this.endTime = endTime;
     this.sleepQuality = sleepQuality;
---
+++ src/main/java/ru/yandex/practicum/sleeptracker/parser/SleepLogParser.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.sleeptracker.parser;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import ru.yandex.practicum.sleeptracker.model.SleepQuality;
+import ru.yandex.practicum.sleeptracker.model.SleepingSession;
+
+import java.io.IOException;
+
+public class SleepLogParser {
+
+    public List<SleepingSession> parse(String path) throws IOException {
+        return Files.lines(Path.of(path))
+                .map(this::parseLine)
+                .toList();
+    }
+
+    private SleepingSession parseLine(String line) {
+        String[] parts = line.split(";");
+
+        LocalDateTime start = parseDate(parts[0]);
+        LocalDateTime end = parseDate(parts[1]);
+        SleepQuality quality = SleepQuality.valueOf(parts[2]);
+
+        return new SleepingSession(start, end, quality);
+    }
+
+    private LocalDateTime parseDate(String str) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
+        return LocalDateTime.parse(str, formatter);
+    }
